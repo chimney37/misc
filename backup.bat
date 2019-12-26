311b79b450ecf11656b259eb579a93c172930ccb
Tetsuaki Otsuki <chimney37@hotmail.com>
2019-12-26 11:08:04 +0900

    Made Batch process more scalable.

--- backup.bat
+++ backup.bat
@@ -6,12 +6,31 @@ rem
 
 
 rem Settings here
+set Sourcedrive=z
+set Destdrive=e
+set ExcludeDirs="LXUSER-PC2" "WindowsImageBackup" "OneDrive" "trashbox"
+set ExcludeFiles="MediaID.bin"
 
+rem exclusion for source directories
+set "DataVar=\iphone_bak,\mai_iphone_bak"
+set "dir_list=\iphone_bak \mai_iphone_bak"
+
+setlocal EnableDelayedExpansion
+for %%s in (%dir_list%) do (
+  set "ExcludeSources=%Sourcedrive%:%%s !ExcludeSources!"
+)
+
+rem Batch routines
+set "Batch1=mirror backup from a drive to a backup drive"
+set "Batch2=unused...may be useful for future batch process"
+
+echo %%Batch1%% %Batch1%
+echo %%Batch2%% %Batch2%
 
 :input
 echo.
 set userkey=
-set /p userkey=End (Enter) / Execute Batch1 (o + Enter) / Execute Batch2 (p + Enter) / Execute Batch1,2 (b + Enter)?
+set /p userkey=End (Enter) / Execute %%Batch1%% (o + Enter) / Execute %%Batch2%% (p + Enter) / Execute All (b + Enter)?
 if not '%userkey%'=='' set userkey=%userkey:~0,1%
 if '%userkey%'=='o' goto first
 if '%userkey%'=='p' goto second
@@ -34,16 +53,16 @@ call :routine_batch2
 pause
 goto quit
 
-:routine_batch1
+:routine_batch1 (backs up external drive content to internal drive)
 :add /MIR if mirror is intended
 echo 'Execute Batch 1'
-robocopy z:\ d:\zbackup /Z /E /XJD /XO /XJF /FFT /LOG:"d:\backup1.log" /NP /TEE
+robocopy %Sourcedrive%:\ %Destdrive%:\zbackup /Z /E /XJD /XO /XJF /XD %ExcludeDirs% /XF %ExcludeFiles% /XD %ExcludeSources% /FFT /LOG:"%Destdrive%:\backup.log" /NP /TEE
 exit /b
 
-:routine_batch2
+:routine_batch2 (unused)
 :add /MIR if mirror is intended
 echo 'Execute Batch 2'
-robocopy d:\temp\pics j:\pics /Z /E /XJD /FFT /XJF /XO /XD "iPod Photo Cache" /LOG:"d:\backup2.log" /NP /TEE
+echo 'This is unused...'
 exit /b
 
 rem Quitting the program
